e989c71ba67d908b6db86381638e13ba688a1558
kimmotikka <kimmo.tikka@fmi.fi>
2017-03-04 16:14:24 +0200

    Create current-positions.js

---
+++ webdirectory/current/current-positions.js
@@ -0,0 +1,11 @@
+[{
+    "lastPosition": [59.91, 19.8518],
+    "lastTime": "2017-02-20 19:25:51",
+    "glider": "sim_uivelo",
+    "mission": "Lagskar2017"
+}, {
+    "lastPosition": [59.7697, 19.948],
+    "lastTime": "2017-02-20 19:14:58",
+    "glider": "s_aland",
+    "mission": "Lagskar2017"
+}]
